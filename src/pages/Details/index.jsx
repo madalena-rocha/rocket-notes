b81a106f2eca0aa83fd0536cd7c406c245fefea7
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-11-15 19:26:38 -0300

    :sparkles: add component button

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,16 @@
 import { Container } from "./styles";
 
+import { Button } from "../../components/Button";
+
 export function Details() {
   return (
     <Container>
       <h1>Hello World!</h1>
       <span>Madalena Machado</span>
+
+      <Button title="Login" loading />
+      <Button title="Cadastrar" />
+      <Button title="Voltar" />
     </Container>
   );
 }
