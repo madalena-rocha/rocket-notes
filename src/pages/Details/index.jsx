649ca915359840b54e5289c0207d44893dee1232
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-11-16 11:30:26 -0300

    :sparkles: add component ButtonText

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,17 @@
 import { Container, Links } from "./styles";
 
-import { Header } from "../../components/Header";
-import { Button } from "../../components/Button";
-import { Section } from "../../components/Section";
 import { Tag } from "../../components/Tag";
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details() {
   return (
     <Container>
 			<Header />
+
+			<ButtonText title="Excluir nota" />
 
 			<Section title="Links úteis">
 				<Links>
