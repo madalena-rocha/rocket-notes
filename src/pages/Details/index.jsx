a33e312670a0008eea2b76ebc0e19056b38de24b
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-11-16 11:00:33 -0300

    :sparkles: add component tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./styles";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
   return (
@@ -14,6 +15,11 @@ export function Details() {
 					<li><a href="#">https://app.rocketseat.com.br/</a></li>
 					<li><a href="#">https://app.rocketseat.com.br/</a></li>
 				</Links>
+			</Section>
+			
+			<Section title="Marcadores">
+				<Tag title="express" />
+				<Tag title="nodejs" />
 			</Section>
 
       <Button title="Voltar" />
