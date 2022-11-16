c6a1b183b8ae96f079902507743bbe2397a33f36
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-11-16 10:49:06 -0300

    :sparkles: add component section

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,22 @@
-import { Container } from "./styles";
+import { Container, Links } from "./styles";
 
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
 
 export function Details() {
   return (
     <Container>
 			<Header />
 
+			<Section title="Links úteis">
+				<Links>
+					<li><a href="#">https://app.rocketseat.com.br/</a></li>
+					<li><a href="#">https://app.rocketseat.com.br/</a></li>
+				</Links>
+			</Section>
+
       <Button title="Voltar" />
     </Container>
-  );
+  ); // o children não é passado como uma propriedade comum, sendo passado dentro do <Section></Section>
 }
