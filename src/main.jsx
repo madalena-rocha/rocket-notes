a78b8e52baaa36d54538b13e47d1107d810ef460
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-11-19 17:10:54 -0300

    :zap: create page SignIn

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
-import { Home } from './pages/Home';
+import { SignIn } from './pages/SignIn';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,39 @@
+import { FiMail, FiLock } from 'react-icons/fi';
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
+import { Container, Form, Background } from './styles';
+
+export function SignIn(){
+  return (
+		<Container>
+			<Form>
+				<h1>Rocket Notes</h1>
+				<p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+				<h2>Faça seu login</h2>
+
+				<Input 
+					placeholder="E-mail"
+					type="text"
+					icon={FiMail}
+				/>
+
+				<Input 
+					placeholder="Senha"
+					type="password"
+					icon={FiLock}
+				/>
+
+				<Button title="Entrar" />
+
+				<a href="#">
+					Criar conta
+				</a>
+			</Form>
+
+			<Background />
+		</Container>
+	);
+}
