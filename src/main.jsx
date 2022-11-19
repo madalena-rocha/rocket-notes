a25498a03ed4bebfee164e5b6b30b2b03b5d3247
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-11-19 17:25:03 -0300

    :zap: create page SignUp

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/global';
 import theme from './styles/theme';
-import { SignIn } from './pages/SignIn';
+import { SignUp } from './pages/SignUp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,45 @@
+import { FiMail, FiLock, FiUser } from 'react-icons/fi';
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+
+import { Container, Form, Background } from './styles';
+
+export function SignUp(){
+  return (
+		<Container>
+			<Background />
+
+			<Form>
+				<h1>Rocket Notes</h1>
+				<p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+				<h2>Crie sua conta</h2>
+
+				<Input 
+					placeholder="Nome"
+					type="text"
+					icon={FiUser}
+				/>
+
+				<Input 
+					placeholder="E-mail"
+					type="text"
+					icon={FiMail}
+				/>
+
+				<Input 
+					placeholder="Senha"
+					type="password"
+					icon={FiLock}
+				/>
+
+				<Button title="Cadastrar" />
+
+				<a href="#">
+					Voltar para o login
+				</a>
+			</Form>
+		</Container>
+	);
+}
