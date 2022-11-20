63ee75a8c0ad61c0e1cca882d4ccec2b9f0da122
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-11-19 23:07:46 -0300

    :zap: finish page new

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,8 +1,9 @@
-import { Header } from '../../components/Header';
-import { Input } from '../../components/Input';
 import { Textarea } from '../../components/Textarea';
 import { NoteItem } from '../../components/NoteItem';
 import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
 
 import { Container, Form } from './styles';
 
@@ -26,6 +27,15 @@ export function New() {
 						<NoteItem value="https://rocketseat.com.br" />
 						<NoteItem isNew placeholder="Novo link" />
 					</Section>
+
+					<Section title="Marcadores">
+						<div className="tags">
+							<NoteItem value="react" />
+							<NoteItem isNew placeholder="Nova tag" />
+						</div>
+					</Section>
+
+					<Button title="Salvar" />
 				</Form>
 			</main>
 		</Container>   
