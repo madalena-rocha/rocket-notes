c7b458b235fb2ceb85fd07cb76c91b826a5c6a80
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-12-07 13:34:38 -0300

    :sparkles: add tag

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -14,6 +14,9 @@ export function New() {
 	const [links, setLinks] = useState([]); // estado para guardar todos os links
 	const [newLink, setNewLink] = useState(""); // estado para guardar o link adicionado
 
+	const [tags, setTags] = useState([]);
+	const [newTag, setNewTag] = useState("");
+
 	function handleAddLink() {
 		setLinks(prevState => [...prevState, newLink])
 		// acessar o estado anterior, pegar o conteúdo armazenado e montar um novo vetor despejando tudo que tinha antes com o novo link
@@ -23,6 +26,15 @@ export function New() {
 	function handleRemoveLink(deleted) { // recebe o link que deseja remover da lista
 		setLinks(prevState => prevState.filter(link => link !== deleted));
 		// retorna a nova lista contendo todos os links da coleção, menos o link que deseja deletar
+	}
+
+	function handleAddTag() {
+		setTags(prevState => [...prevState, newTag])
+		setNewTag("");
+
+		// ["react", "nodejs"] => [["react", "nodejs"], "express"]
+		// prevState => [...prevState, newTag]
+		// ["react", "nodejs"] => ["react", "nodejs", "express"]
 	}
 
   	return (
@@ -63,8 +75,23 @@ export function New() {
 
 					<Section title="Marcadores">
 						<div className="tags">
-							<NoteItem value="react" />
-							<NoteItem isNew placeholder="Nova tag" />
+							{ // usando chaves porque vai usar conteúdo de uma variável, no caso uma lista, para percorrê-la
+								tags.map((tag, index) => (
+									<NoteItem 
+										key={String(index)}
+										value={tag} 
+										onClick={() => { }}
+									/>
+								))
+							}
+
+							<NoteItem 
+								isNew 
+								placeholder="Nova tag" 
+								onChange={e => setNewTag(e.target.value)}
+								value={newTag}
+								onClick={handleAddTag}
+							/>
 						</div>
 					</Section>
 
