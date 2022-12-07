35add45430797b55270738703b046bc78ac07010
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-12-07 11:19:01 -0300

    :zap: using local storage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react'; // context api do React
+import { createContext, useContext, useState, useEffect } from 'react'; // context api do React
 
 import { api } from '../services/api';
 
@@ -13,6 +13,13 @@ function AuthProvider({ children }) {
             const response = await api.post("/sessions", { email, password });
             const { user, token } = response.data;
 
+            // evitar que mesmo com o usuário autentivcado a aplicação volte para a tela de login ao atualizar a página
+            // até agora, os dados do usuário autenticado estão guardados num estado, que é recarregado com o reload da página
+            // armazenar essas informações no storage do navegador
+            // buscar essas informações no local storage quando o usuário recarregar a página e preencher o estado para refletir nos lugares que estão usando o estado
+            localStorage.setItem("@rocketnotes:user", JSON.stringify(user)); // JSON.stringify para converter o objeto em texto
+            localStorage.setItem("@rocketnotes:token", token);
+
             api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
             // inserindo um token do tipo bearer de autorização no cabeçalho por padrão de todas as requisições que o usuário fizer
             setData({ user, token });
@@ -24,6 +31,25 @@ function AuthProvider({ children }) {
             }
         }
     }
+
+    useEffect(() => {
+        const token = localStorage.getItem("@rocketnotes:token");
+        const user = localStorage.getItem("@rocketnotes:user");
+
+        if(token && user) {
+            // garantir que tanto o token quanto o usuário tenham sido informados
+            api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+            setData({
+                token,
+                user: JSON.parse(user)
+                // pegando os dados do usuário armazenados em formato de texto e retornando para um objeto do tipo JSON
+            });
+        }
+    }, []);
+    // a primeira parte é uma arrow function, correspondendo ao que deseja executar após a renderização do componente
+    // a segunda parte é um vetor que pode colocar um estado, e quando esse estado mudar, dispara o useEffect novamente
+    // vetor de dependências vazio, será carregado uma vez após a renderização do componente
 
     return (
         <AuthContext.Provider value={{ signIn, user: data.user }}>
