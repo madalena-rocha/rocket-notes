86031d40d60fde8d76d0f8548171db9e65cb97aa
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-12-07 13:46:07 -0300

    :sparkles: validate note fields

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -51,6 +51,18 @@ export function New() {
 	}
 
 	async function handleNewNote() {
+		if (!title) {
+			return alert("Digite o título da nota");
+		}
+		
+		if (newLink) {
+			return alert("Você deixou um link no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe o campo vazio.");
+		}
+		
+		if (newTag) {
+			return alert("Você deixou uma tag no campo para adicionar, mas não clicou em adicionar. Clique para adicionar ou deixe o campo vazio.");
+		}
+
 		await api.post("/notes", {
 			title,
 			description,
